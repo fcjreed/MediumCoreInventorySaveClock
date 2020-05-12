4decc03eb491210db1c24a6b5ab35c42f727e0c7
fcjreed <fcjreed@gmail.com>
2020-05-12 22:32:06 +0100

    Remove check that already exists

--- MediumCoreInventorySaveClock.cs
+++ MediumCoreInventorySaveClock.cs
@@ -19,25 +19,26 @@ namespace MediumCoreInventorySaveClock
         }
         
          public override void ModifyInterfaceLayers(List<GameInterfaceLayer> layers)
-		{
+        {
             Config cfg = ModContent.GetInstance<Config>();
             int mouseText = layers.FindIndex(layer => layer.Name.Equals("Vanilla: Mouse Text"));
-			if (cfg.clockEnabled && mouseText != -1)
-			{
-				layers.Insert(mouseText, new LegacyGameInterfaceLayer("RealTime: Player Name", delegate
-				{
+            if (cfg.clockEnabled && mouseText != -1)
+            {
+                layers.Insert(mouseText, new LegacyGameInterfaceLayer("RealTime: Player Name", delegate
+                {
                     string text = DateTime.Now.ToString(cfg.clockFormat);
                     int offset = cfg.offset;
                     Vector2 overridePosition = cfg.position;
                     Vector2 position = overridePosition.Y != -1 ? overridePosition * new Vector2(Main.screenWidth, Main.screenHeight) : new Vector2(Main.miniMapWidth * 0.7f + Main.miniMapX, Main.miniMapY);
-                    if (Main.mapStyle == 1 && overridePosition.Y != -1)  {
+                    if (Main.mapStyle == 1)
+                    {
                         position = overridePosition.Y != -1 ? overridePosition * new Vector2(Main.screenWidth, Main.screenHeight) : new Vector2(Main.miniMapWidth * 0.7f + Main.miniMapX, Main.miniMapHeight + Main.miniMapY + offset);
                     }
                     Utils.DrawBorderString(Main.spriteBatch, text, position, Color.WhiteSmoke);
-					return true;
-				}, InterfaceScaleType.UI));
-			}
-		}
+                    return true;
+                }, InterfaceScaleType.UI));
+            }
+        }
 
         public override void Load()
         {
